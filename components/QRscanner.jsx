dd04c3ad338851dd4dd16c834911aabadae58893
Shrivatsa255 <shreevathsa255@gmail.com>
2024-01-07 13:03:17 +0530

    QR Code scanning with media constraints

--- components/QRscanner.jsx
+++ components/QRscanner.jsx
@@ -1,6 +1,6 @@
 // components/QRScanner.js
 import React, { useState } from "react";
-import QrScanner from "react-qr-scanner"
+import QrScanner from "react-qr-scanner";
 
 const QRScanner = () => {
   const [data, setData] = useState(null);
@@ -16,11 +16,15 @@ const QRScanner = () => {
       setScanning(false);
     }
   };
+  const videoConstraints = {
+    facingMode: "environment", // Use the back camera
+  };
 
   return (
     <div className="rounded-md p-7 bg-nft-dark-3 dark:border-indigo-100 shadow-lg">
       {scanning && (
         <QrScanner
+          videoConstraints={videoConstraints}
           onScan={handleScan}
           onError={handleError}
           style={{ width: "100%" }}
