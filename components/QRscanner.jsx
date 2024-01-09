c7f5ff85e0174183395ffbf1d0da2ac7daa09b8b
Shrivatsa255 <shreevathsa255@gmail.com>
2024-01-10 00:13:36 +0530

    Revert to back

--- components/QRscanner.jsx
+++ components/QRscanner.jsx
@@ -5,8 +5,6 @@ import QrReader from "react-qr-scanner";
 const QRScanner = () => {
   const [data, setData] = useState(null);
   const [scanning, setScanning] = useState(true);
-  const [availableCameras, setAvailableCameras] = useState([]);
-  const [selectedCamera, setSelectedCamera] = useState(null);
 
   const handleError = (error) => {
     console.error("QR Code Scanner Error:", error);
@@ -19,50 +17,8 @@ const QRScanner = () => {
     }
   };
 
-  const getAvailableCameras = async () => {
-    try {
-      const devices = await navigator.mediaDevices.enumerateDevices();
-      const cameras = devices.filter((device) => device.kind === "videoinput");
-      setAvailableCameras(cameras);
-      if (cameras.length > 0) {
-        setSelectedCamera(cameras[0].deviceId);
-      }
-    } catch (error) {
-      console.error("Error getting available cameras:", error);
-    }
-  };
-
-  useEffect(() => {
-    getAvailableCameras();
-  }, []);
-
-  const handleCameraChange = (event) => {
-    setSelectedCamera(event.target.value);
-    setScanning(true);
-  };
-
   return (
     <div className="rounded-md p-7 bg-nft-dark-3 dark:border-indigo-100 shadow-lg">
-      {availableCameras.length > 1 && (
-        <div className="mb-4">
-          <label htmlFor="cameraSelect" className="text-white">
-            Choose Camera:
-          </label>
-          <select
-            id="cameraSelect"
-            className="ml-2 p-2 border rounded-md"
-            value={selectedCamera}
-            onChange={handleCameraChange}
-          >
-            {availableCameras.map((camera) => (
-              <option key={camera.deviceId} value={camera.deviceId}>
-                {camera.label || `Camera ${camera.deviceId.slice(-1)}`}
-              </option>
-            ))}
-          </select>
-        </div>
-      )}
-
       {scanning && (
         <QrReader
           constraints={{
